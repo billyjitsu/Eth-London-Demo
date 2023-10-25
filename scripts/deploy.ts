a57ca6ebd1f67fc3c6663dd4a704b323ea281181
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-10-24 21:44:33 -0700

    update deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,21 +1,15 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const airnode = "0x2ab9f26E18B64848cd349582ca3B55c2d06f507d" // Airnode sepolia
+  // The new way of deploying contracts    Name of Contract, Constructor Arguments, Overrides
+  const tokenEx = await ethers.deployContract("QrngExample", [airnode], {});
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
+  await tokenEx.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `TokenEx contract address: ${tokenEx.target}`
   );
 }
 
